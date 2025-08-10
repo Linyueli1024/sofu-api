12f38e4aca2a7d2491804be30731bc4ffef3ca88
Linyueli1024 <Linyueli1024@163.com>
2025-08-10 22:46:10 +0100

    fix：添加身份验证中间件-like

--- routes/like.js
+++ routes/like.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { toggleLike, getLikesByUser } from "../controllers/like.js";
+import { authMiddleware } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.post("/like", toggleLike);
+router.post("/like", authMiddleware, toggleLike);
 router.get("/like/:userId", getLikesByUser);
 
 export default router;
